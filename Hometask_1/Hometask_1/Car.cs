d912abf31d0970fb1083305dff2470d62adcf0fa
Komissaari <zandrmerte02@gmail.com>
2023-04-16 17:34:45 +0300

    a

--- Hometask_1/Hometask_1/Car.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Hometask_1
-{
-    public class Car
-    {
-        public string CarBrand { get; set; }
-        public string CarName { get; set; }
-        public string CarType { get; set; }
-    }
-}
