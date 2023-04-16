226dff95c37c4b3a43323c6a3c2107d4ee310d57
Komissaari <zandrmerte02@gmail.com>
2023-04-16 17:36:14 +0300

    Hometassk_1

---
+++ Hometask_1/Serializ/MyExtentionDeSerialization.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+
+namespace ExtentionDeSerialization
+{
+    public static class MyExtentionDeSerialization
+    {
+        public static T ExtDeSer<T>(this string json)
+        {
+
+            return JsonSerializer.Deserialize<T>(json);
+        }
+    }
+}
