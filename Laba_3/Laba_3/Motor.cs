93c82aa8f39b1fc443a203a1309ae53a3a7e1cb8
Komissaari <zandrmerte02@gmail.com>
2023-04-22 15:31:40 +0300

    Laba_3

---
+++ Laba_3/Laba_3/Motor.cs
@@ -0,0 +1,17 @@
+﻿
+namespace _Motor_
+{
+    public delegate void MyEvent();
+    public class Motor
+    {
+        public Motor() { }
+        public MyEvent _Event; 
+
+        public void Event() 
+        {
+            MyEvent allevent = _Event;
+            if (allevent != null)
+                allevent();
+        }
+    }
+}
---
+++ Laba_3/Laba_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using _Motor_;
+
+namespace Laba_3
+{
+    internal partial class Program
+    { 
+    
+    static void Main(string[] args)
+    {
+        Motor motor = new Motor();
+        motor._Event += () => Console.WriteLine("Электричество"); 
+        motor._Event += () => Console.WriteLine("Бензин");
+        motor._Event += () => Console.WriteLine("Дизель");
+        motor.Event(); 
+        Console.ReadKey();
+    }
+    }
+}
