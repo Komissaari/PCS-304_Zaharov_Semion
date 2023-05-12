be8cde3f161d29b687c4a76629f8c4f57aa8d3f7
Komissaari <124903323+Komissaari@users.noreply.github.com>
2023-05-12 21:57:04 +0300

    Laba_5

---
+++ Laba_5/Laba_5/Car.cs
@@ -0,0 +1,15 @@
+﻿namespace Laba_5
+{
+    public static class Car
+    {
+        public static string CarName { get; set; }
+        public static string CarType { get; set; }
+        public static string CarModel { get; set; }
+        static Car()
+        {
+            CarName = "Vesta";
+            CarType = "Sedan";
+            CarModel = "Lada";
+        }
+    }
+}
